4ca8508c8cdf0042de79952756da508554eb0386
Alexander Kaznacheev <aakzn@mail.ru>
2015-03-24 11:12:13 +0300

    Добавлены комментарии и оформлен вывод данных на экран

--- src/CalculationWord.java
+++ src/CalculationWord.java
@@ -15,35 +15,48 @@ import java.io.*;
 public class CalculationWord {
     public static void main( String[] args )
             throws FileNotFoundException, IOException {
-        boolean pos;
-        int pril=0,gl=0,nar=0;
-        List<String> list = new ArrayList<String>();
-        Scanner in = new Scanner(new File("C:\\Users\\IKZN\\IdeaProjects\\pp_lab3\\src\\text.txt"));
-        while (in.hasNextLine())
+
+        boolean pos;        //переменная реагирующая на наличие подслова
+        int pril=0;         //количество прилагательных
+        int gl=0;           //количество глаголов
+        int nar=0;          //количство наречий
+
+        List<String> list = new ArrayList<String>();        //создаем набор стринговских переменных
+        Scanner in = new Scanner(new File("C:\\Users\\IKZN\\IdeaProjects\\pp_lab3\\src\\text.txt")); //открываем файл
+
+        while (in.hasNextLine())                            //читаем
             list.add(in.nextLine());
-        String[] array = list.toArray(new String[0]);
 
-        System.out.println(array[0]);
+        String[] array = list.toArray(new String[0]);       //создаем массив стринговских переменных
 
-        String parts[] = array[0].split(" ");
+        System.out.println(array[0]);                       //выводим текст
 
-        for(int i=0;i!=parts.length;i++) {
+        String parts[] = array[0].split(" ");               //разбиваем его на слова
+
+        for(int i=0;i!=parts.length;i++) {                  //ищем прилогательные по окончаниям
             if ((pos = parts[i].matches("(.*)(ый|ой|ее|ий|его|ему|ого|им|ому)")) != false) {
                 pril++;
             }
         }
-        for(int i=0;i!=parts.length;i++) {
-            if ((pos = parts[i].matches("(.*)(ал|ла|ай|ть|аю|ши|рь|ет)")) != false) {
+        for(int i=0;i!=parts.length;i++) {                  //ищем глаголы
+            if ((pos = parts[i].matches("(.*)(ал|ла|ай|ть|аю|ши|рь|ет|шь)")) != false) {
                 gl++;
             }
         }
-        for(int i=0;i!=parts.length;i++) {
+        for(int i=0;i!=parts.length;i++) {                  //ищем наречия
             if ((pos = parts[i].matches("(.*)(но|ше|ом|ём|ро|нно|ро)")) != false) {
                 nar++;
             }
         }
+
+        //выводим все
+        System.out.println("    Количество слов:");
+        System.out.println(parts.length);
+        System.out.println("    Количество прилагательных:");
         System.out.println(pril);
+        System.out.println("    Количество глаголов:");
         System.out.println(gl);
+        System.out.println("    Количество наречий:");
         System.out.println(nar);
        /* try(FileReader reader = new FileReader("C:\\Users\\IKZN\\IdeaProjects\\pp_lab3\\src\\text.txt"))
         {
